45b51324da4056e71cd8f86283af0cad52e757ec
abhishek chaudhary <abhishek0221997@gmail.com>
2026-03-13 11:38:12 +0530

    main.app

--- day4/main.js
+++ day4/main.js
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const domRoot=document.getElementById("root");
-const reactRoot=ReactDOM.createRoot(domRoot);
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
+const domRoot = document.getElementById("root");
+const reactRoot = ReactDOM.createRoot(domRoot);
 
-const App=()=>{
-    return(
-        <div>
-            <h1>hello shurti shukla</h1>
-        </div>
-    );
+
+const App = () => {
+  return (
+    <div>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </div>
+  );
 };
 reactRoot.render(App());
